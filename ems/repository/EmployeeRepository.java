0426e111df01f11cfc84f749bab4b7e69d1b8eaf
Saahil Sorakayala <saahilsorakayala@gmail.com>
2024-10-03 14:17:55 -0500

    Delete ems directory

--- ems/repository/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package net.javaguides.ems.repository;
-// This class extends JpaRepository to get CRUD methods
-// to perform CRUD database operations on the Employee JPA entity
-import net.javaguides.ems.entity.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-
-}
